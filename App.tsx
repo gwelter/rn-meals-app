d5b43bbdd7e9eda315afa6b4a000c4d4c8bf78a5
Guilherme André Welter <guilherme.a.welter@gmail.com>
2020-02-22 11:58:15 -0300

    Loading font and organizing imports

--- App.tsx
+++ App.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from "expo";
+import * as Font from "expo-font";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+function fetchFonts() {
+  return Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf")
+  });
+}
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+      />
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -12,8 +31,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
