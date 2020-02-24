308954854939043826ba7197c6d77fd8e17cd193
Guilherme André Welter <guilherme.a.welter@gmail.com>
2020-02-24 16:05:30 -0300

    Fixing styling

--- src/components/CategoryGridTile.tsx
+++ src/components/CategoryGridTile.tsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from "react-native";
 
 interface Props {
   onSelect: () => void;
@@ -30,21 +36,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     borderRadius: 10,
+    padding: 15,
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
     shadowColor: "black",
     shadowOpacity: 0.26,
     shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 10,
-    elevation: 3,
-    padding: 15,
-    justifyContent: "flex-end",
-    alignItems: "flex-end"
+    shadowRadius: 10
   },
   gridItem: {
     flex: 1,
     margin: 15,
     height: 150,
     borderRadius: 10,
-    overflow: "hidden"
+    overflow:
+      Platform.OS === "android" && Platform.Version >= 21
+        ? "hidden"
+        : "visible",
+    elevation: 5
   },
   title: {
     fontFamily: "open-sans-bold",
